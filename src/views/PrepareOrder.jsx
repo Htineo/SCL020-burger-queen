ac4daf0749fc0baa53e250a9a210bb3193b9c151
Heike Tineo <tineo.cruz.heike@gmail.com>
2022-08-04 07:18:19 -0500

    :tada: actualizando cambios

--- src/views/PrepareOrder.jsx
+++ src/views/PrepareOrder.jsx
@@ -2,26 +2,163 @@ import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
-import Layout from "./Layout";
 
 function PrepareOrder() {
   return (
     <>
-      <Layout>
-        <header className="bg-white shadow">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-900">Servir Pedido</h1>
-          </div>
-        </header>
-        <main>
-          <h2></h2>
-          <p>That feels like an existential question, don't you think?</p>
-        </main>
-        <nav>
-          <Link to="/home">Home</Link>
-          <Link to="/">Cerrar Sesión</Link>
-        </nav>
-      </Layout>
+      <header>
+        <ul>
+          <li>
+            <a href="">Servir</a>
+          </li>
+          <li>
+            <a href="">Perfil</a>
+          </li>
+          <li>
+            <a href="">Cerrar sesión</a>
+          </li>
+        </ul>
+      </header>
+      <div class="cont-card">
+        <div class="card">
+          <h3>Arepa Reina Pepiada</h3>
+          <img src="https://i.postimg.cc/2y4VJJSc/arepa.png" alt="" />
+          <span class="price">$1.200</span>
+        </div>
+        <div class="card">
+          <h3>Arepa Reina Pepiada</h3>
+          <img src="https://i.postimg.cc/2y4VJJSc/arepa.png" alt="" />
+          <span class="price">$1.200</span>
+        </div>
+        <div class="card">
+          <h3>Arepa Reina Pepiada</h3>
+          <img src="https://i.postimg.cc/2y4VJJSc/arepa.png" alt="" />
+          <span class="price">$1.200</span>
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+
+      <section class="section-menu">
+        <table class="table-menu">
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Arepa Reina Pepiada</td>
+              <td>$1.200</td>
+              <td>
+                <div class="counter">
+                  <button>
+                    <img src="https://i.postimg.cc/hPydq8tr/menos.png" alt="" />
+                  </button>
+                  <span>2</span>
+                  <button>
+                    <img src="https://i.postimg.cc/x8kSNSLq/mas.png" alt="" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <button>
+                  <img
+                    src="https://i.postimg.cc/NG86ZrXJ/eliminar.png"
+                    alt=""
+                  />
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Arepa Reina Pepiada</td>
+              <td>$1.200</td>
+              <td>
+                <div class="counter">
+                  <button>
+                    <img src="https://i.postimg.cc/hPydq8tr/menos.png" alt="" />
+                  </button>
+                  <span>2</span>
+                  <button>
+                    <img src="https://i.postimg.cc/x8kSNSLq/mas.png" alt="" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <button>
+                  <img
+                    src="https://i.postimg.cc/NG86ZrXJ/eliminar.png"
+                    alt=""
+                  />
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Arepa Reina Pepiada</td>
+              <td>$1.200</td>
+              <td>
+                <div class="counter">
+                  <button>
+                    <img src="https://i.postimg.cc/hPydq8tr/menos.png" alt="" />
+                  </button>
+                  <span>2</span>
+                  <button>
+                    <img src="https://i.postimg.cc/x8kSNSLq/mas.png" alt="" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <button>
+                  <img
+                    src="https://i.postimg.cc/NG86ZrXJ/eliminar.png"
+                    alt=""
+                  />
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Arepa Reina Pepiada</td>
+              <td>$1.200</td>
+              <td>
+                <div class="counter">
+                  <button>
+                    <img src="https://i.postimg.cc/hPydq8tr/menos.png" alt="" />
+                  </button>
+                  <span>2</span>
+                  <button>
+                    <img src="https://i.postimg.cc/x8kSNSLq/mas.png" alt="" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <button>
+                  <img
+                    src="https://i.postimg.cc/NG86ZrXJ/eliminar.png"
+                    alt=""
+                  />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <hr />
+
+        <div class="total">
+          <h4>Total:</h4>
+          <span>$10.40</span>
+        </div>
+        <div class="options">
+          <a class="btn" href="">
+            COCINAR
+          </a>
+        </div>
+      </section>
     </>
   );
 }

b4a954e3255d1bdadbd7cbffe87f095d57fbb7a9
Heike Tineo <tineo.cruz.heike@gmail.com>
2022-08-10 10:45:34 -0500

    :tada: actualizando cambios de menu y lista de productos

--- src/views/PrepareOrder.jsx
+++ src/views/PrepareOrder.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
-import menuPeruvian from '../data/menuPeruvian.json'
-
+import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { Disclosure, Menu, Transition } from "@headlessui/react";
+// import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import menuPeruvian from "../data/menuPeruvian.json";
+import menuVenezuelan from "../data/menuVenezuelan.json";
+import menuAggregates from "../data/menuAggregates.json";
 import "./PrepareOrder.css";
-import menuPeruano from "../data/data.json";
-import menuVenezolano from "../data/data2.json";
 
 import {
   Tabs,
@@ -19,18 +18,76 @@ import {
 const data = [
   {
     label: "Menú Peruano ",
-    value: "MenuPeruano",
+    value: "menuPeruvian",
   },
   {
     label: "Menú Venezolano",
-    value: "MenuVenezolano",
+    value: "menuVenezuelan",
+  },
+  {
+    label: "Menú Agregados ",
+    value: "menuAggregates",
   },
 ];
 
 const MenuView = () => {
-  
- 
-  return (<>
+  const [list, setList] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  // Función donde se escucha el click que se hace a cada plato
+  const cardClick = (value) => {
+    console.log("Detectando click", value);
+    let duplicate = list.filter((item) => item.id === value.id);
+    if (duplicate.length === 0) {
+      value.counter = 1;
+      setList([...list, value]);
+      console.log("value.precio ", value.precio, value.counter);
+      let sumaClick = value.price * value.counter;
+      setSum(sum + sumaClick);
+    }
+  };
+
+  const less = (id) => {
+    let update = list.map((item) => {
+      if (item.id === id) {
+        item.counter -= 1;
+        return item;
+      }
+      return item;
+    });
+    setList(update);
+    sumTotal();
+  };
+  const more = (id) => {
+    let update = list.map((item) => {
+      if (item.id === id) {
+        item.counter += 1;
+        return item;
+      }
+      return item;
+    });
+    setList(update);
+    sumTotal();
+  };
+
+  const remove = (id) => {
+    let update = list.filter((item) => item.id !== id);
+    setList(update);
+    sumTotal();
+  };
+
+  const sumTotal = () => {
+    let suma = 0;
+    list.map((item) => {
+      suma += item.price * item.counter;
+    });
+    console.log(suma, "sumaaa", list);
+    setSum(suma);
+  };
+
+  // Botones de redirección a las vistas
+  return (
+    <>
       <header>
         <ul>
           <li>
@@ -45,20 +102,65 @@ const MenuView = () => {
         </ul>
       </header>
 
-      <div className="cont-card">
-        {
-          menuPeruvian.map( (value, key) =>{
-            return(
+      {/* Componente de los tres menús */}
 
-            <div className="card" key={key+ 1}>
-              <h3>{value.name}</h3>
-              <img src={value.img} alt="" />
-              <span className="price">{value.price}</span>
+      {/* componentes de tailwind: es una tabla de 1x3 */}
+      <Tabs value="menu">
+        <TabsHeader>
+          {data.map(({ label, value }) => (
+            <Tab key={value} value={value}>
+              {label}
+            </Tab>
+          ))}
+        </TabsHeader>
+        <TabsBody>
+          <TabPanel key={"menuPeruvian"} value={"menuPeruvian"}>
+            <div class="cont-card">
+              {menuPeruvian.map((value, key) => {
+                return (
+                  <div
+                    className="card"
+                    key={key + 1}
+                    onClick={() => cardClick(value)}
+                  >
+                    <h3>{value.name}</h3>
+                    <img src={value.img} alt="" />
+                    <span className="price">{value.price}</span>
+                  </div>
+                );
+              })}
             </div>
-            )
-          })
-        }
-      </div>
+          </TabPanel>
+
+          <TabPanel key={"menuVenezuelan"} value={"menuVenezuelan"}>
+            <div class="cont-card">
+              {menuVenezuelan.map((value, key) => {
+                return (
+                  <div className="card" key={key + 1}>
+                    <h3>{value.name}</h3>
+                    <img src={value.img} alt="" />
+                    <span className="price">{value.price}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </TabPanel>
+
+          <TabPanel key={"menuAggregates"} value={"menuAggregates"}>
+            <div class="cont-card">
+              {menuAggregates.map((value, key) => {
+                return (
+                  <div className="card" key={key + 1}>
+                    <h3>{value.name}</h3>
+                    <img src={value.img} alt="" />
+                    <span className="price">{value.price}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </TabPanel>
+        </TabsBody>
+      </Tabs>
 
       <br />
       <br />
@@ -68,124 +170,67 @@ const MenuView = () => {
         <table className="table-menu">
           <thead>
             <tr>
-              <th></th>
-              <th></th>
-              <th></th>
+              <th>Nombre</th>
+              <th>Precio</th>
+              <th>Sub total</th>
               <th></th>
             </tr>
           </thead>
 
           <tbody>
-            <tr>
-              <td>Arepa Reina Pepiada</td>
-              <td>$1.200</td>
-              <td>
-                <div className="counter">
-                  <button>
-                    <img src="https://i.postimg.cc/hPydq8tr/menos.png" alt="" />
-                  </button>
-                  <span>2</span>
-                  <button>
-                    <img src="https://i.postimg.cc/x8kSNSLq/mas.png" alt="" />
-                  </button>
-                </div>
-              </td>
-              <td>
-                <button>
-                  <img
-                    src="https://i.postimg.cc/NG86ZrXJ/eliminar.png"
-                    alt=""
-                  />
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>Arepa Reina Pepiada</td>
-              <td>$1.200</td>
-              <td>
-                <div className="counter">
-                  <button>
-                    <img src="https://i.postimg.cc/hPydq8tr/menos.png" alt="" />
-                  </button>
-                  <span>2</span>
-                  <button>
-                    <img src="https://i.postimg.cc/x8kSNSLq/mas.png" alt="" />
-                  </button>
-                </div>
-              </td>
-              <td>
-                <button>
-                  <img
-                    src="https://i.postimg.cc/NG86ZrXJ/eliminar.png"
-                    alt=""
-                  />
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>Arepa Reina Pepiada</td>
-              <td>$1.200</td>
-              <td>
-                <div className="counter">
-                  <button>
-                    <img src="https://i.postimg.cc/hPydq8tr/menos.png" alt="" />
-                  </button>
-                  <span>2</span>
-                  <button>
-                    <img src="https://i.postimg.cc/x8kSNSLq/mas.png" alt="" />
-                  </button>
-                </div>
-              </td>
-              <td>
-                <button>
-                  <img
-                    src="https://i.postimg.cc/NG86ZrXJ/eliminar.png"
-                    alt=""
-                  />
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>Arepa Reina Pepiada</td>
-              <td>$1.200</td>
-              <td>
-                <div className="counter">
-                  <button>
-                    <img src="https://i.postimg.cc/hPydq8tr/menos.png" alt="" />
-                  </button>
-                  <span>2</span>
-                  <button>
-                    <img src="https://i.postimg.cc/x8kSNSLq/mas.png" alt="" />
-                  </button>
-                </div>
-              </td>
-              <td>
-                <button>
-                  <img
-                    src="https://i.postimg.cc/NG86ZrXJ/eliminar.png"
-                    alt=""
-                  />
-                </button>
-              </td>
-            </tr>
+            {list.map((item, index) => {
+              return (
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>{list[index].price * list[index].counter} </td>
+                  <td>
+                    <div className="counter">
+                      <button onClick={() => less(item.id)}>
+                        <img
+                          src="https://i.postimg.cc/hPydq8tr/menos.png"
+                          alt=""
+                        />
+                      </button>
+                      <span>{item.counter}</span>
+                      <button onClick={() => more(item.id)}>
+                        <img
+                          src="https://i.postimg.cc/x8kSNSLq/mas.png"
+                          alt=""
+                        />
+                      </button>
+                    </div>
+                  </td>
+                  <td>
+                    <button onClick={() => remove(item.id)}>
+                      <img
+                        src="https://i.postimg.cc/NG86ZrXJ/eliminar.png"
+                        alt=""
+                      />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
         <hr />
-
+        <br />
         <div className="total">
           <h4>Total:</h4>
-          <span>$10.40</span>
+          <span>{sum}</span>
         </div>
+        <br />
         <div className="options">
           <a className="btn" href="">
             COCINAR
           </a>
         </div>
       </section>
+      <br />
+      <br />
+      <br />
     </>
   );
 };
 export default MenuView;
-
-
-
